911eeeb3182407874e00618b13ee400a34619a49
tutuca2003 <dferacosta@hotmail.com>
2026-04-06 22:17:22 -0300

    setting 2

--- bull/settings.py
+++ bull/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'bull.wsgi.application'
 # -----------------------
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', f"postgres://postgres:password@localhost:5432/{BASE_DIR / 'db.sqlite3'}"),
+        default=os.environ.get('DB_EXTERNAL_URL', f"postgres://postgres:password@localhost:5432/{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
         ssl_require=True  # Supabase requiere SSL
     )
